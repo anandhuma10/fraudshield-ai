d5bd1efe5dec5be03226c67d07278a709a3d9426
Anandhu <anandhuma468@gmail.com>
2026-05-28 14:09:19 +0530

    Upgraded AI chatbot UI with typing indicator and chat improvements

--- backend/ai_assistant/views.py
+++ backend/ai_assistant/views.py
@@ -5,14 +5,10 @@ from django.views.decorators.csrf import csrf_exempt
 
 from transactions.models import Transaction
 
-# Create your views here.
 
 def chat_page(request):
+    return render(request, 'ai_assistant/chat.html')
 
-    return render(
-        request,
-        'ai_assistant/chat.html'
-    )
 
 @csrf_exempt
 def chatbot(request):
@@ -21,54 +17,53 @@ def chatbot(request):
 
         data = json.loads(request.body)
 
-        user_message = data.get(
-            'message',
-            ''
-        ).lower()
+        user_message = data.get('message', '').lower()
 
         total = Transaction.objects.count()
-
-        fraud = Transaction.objects.filter(
-            is_fraud=True
-        ).count()
-
+        fraud = Transaction.objects.filter(is_fraud=True).count()
         safe = total - fraud
 
-        # AI Responses
+        # AI Logic
+        if (
+            'fraud' in user_message or
+            'unsafe' in user_message or
+            'scam' in user_message
+        ):
+            reply = f"There are {fraud} fraud transactions."
 
-        if 'fraud' in user_message:
-            reply = (
-                f"There are {fraud} "
-                f"fraud transactions."
-            )
+        elif (
+            'safe' in user_message or
+            'good' in user_message or
+            'secure' in user_message
+        ):
+            reply = f"There are {safe} safe transactions."
 
-        elif 'safe' in user_message:
-            reply = (
-                f"There are {safe} "
-                f"safe transactions."
-            )
+        elif (
+            'total' in user_message or
+            'all' in user_message
+        ):
+            reply = f"Total transactions are {total}."
 
-        elif 'total' in user_message:
-            reply = (
-                f"Total transactions are {total}."
-            )
+        elif (
+            'hello' in user_message or
+            'hi' in user_message
+        ):
+            reply = "Hello! I am FraudShield AI Assistant."
 
         else:
             reply = (
-                "I am FraudShield AI Assistant. "
-                "Ask me about fraud, safe, "
-                "or total transactions."
+                "Sorry, I did not understand. "
+                "Ask me about fraud, safe, or total transactions."
             )
 
         return JsonResponse({
-            'user_message': user_message,
-            'reply': reply
+            "user_message": user_message,
+            "reply": reply,
+            "total": total,
+            "fraud": fraud,
+            "safe": safe
         })
 
     return JsonResponse({
-        'error': 'POST request required'
+        "error": "POST request required"
     })
-
-
-
-
